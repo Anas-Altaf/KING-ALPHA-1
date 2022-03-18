631df96cd11eca23026445f6103f00c30a2e6538
KING ALPHA <101397009+AlphaNinjaReal@users.noreply.github.com>
2022-03-18 21:54:07 +0530

    Delete TrexTeam.js

--- TrexTeam.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    ALPHA_OWNER: '94741209621', // Trex Team - 𝙆𝙄𝙉𝙂 𝘼𝙇𝙋𝙃𝘼 Owner
-    TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
-    TREX_OWNER: '94741209621' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Alpha 
-};
